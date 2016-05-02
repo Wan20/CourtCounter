d36ca7d491db16bf51b66c0d1d7b7dc3f0a0124d
Wan20 <wandy20number@gmail.com>
2016-05-02 22:51:59 +0700

    Logic

--- app/src/main/java/com/example/wansu/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/wansu/courtcounter/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.wansu.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int scoreTeamA = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+
+
     }
     /**
      * Displays the given score for Team A.
@@ -21,26 +23,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given score  1 for Team A.
+     * Increment the given score 1 for Team A.
      */
-    public void OnePOINT(int score) {
-        TextView One = (TextView) findViewById(R.id.onepoint);
-        One.setText(String.valueOf(One));
+    public void OnePOINT(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Increment the given score 2 for Team A.
+     */
+    public void TwoPOINT(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Displays the given score  1 for Team A.
+     * Increment the given score 3 for Team A.
      */
-    public void TwoPOINT(int score) {
-        TextView Two = (TextView) findViewById(R.id.twopoint);
-        Two.setText(String.valueOf(Two));
-    }
-
-    /**
-     * Displays the given score  1 for Team A.
-     */
-    public void ThreePOINT(int score) {
-        TextView Three = (TextView) findViewById(R.id.threepoint);
-        Three.setText(String.valueOf(Three));
+    public void ThreePOINT(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 }
